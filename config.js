e5f03e6401225adad061a2f402b54cc4ec49d3eb
IAM <iam.wfsa@gmail.com>
2026-09-11 07:00:11 +0700

    update: ganti nomor WA admin caten jadi 628131409199

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.APP_CONFIG = {
     ENABLE_WA_BUTTON: true,
 
     // Nomor WA tujuan (Admin Caten)
-    TARGET_PHONE: '6287730085001',
+    TARGET_PHONE: '628131409199',
 
     // Google Sheets rekap endpoint (Apps Script Web App URL)
     SHEETS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbyj59VTDw3COyTULMslV2Ztu0zp2g6fYP2MiJEtprvEet37-vinEjGK6H3x9rbeI8E/exec',
